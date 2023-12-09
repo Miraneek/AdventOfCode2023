268a65a7f49ff876dfc6717217cc3fb540538eca
Miraneek <surykata05@gmail.com>
2023-12-09 18:46:34 +0100

    The nightmare called day 2 (I forgot trim())

--- src/day02/day02part2.java
+++ src/day02/day02part2.java
@@ -4,7 +4,7 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 
-public class day02 {
+public class day02part2 {
     public static File file = new File("src/day02/input.txt");
     public static Scanner scanner;
     static {
@@ -21,49 +21,33 @@ public class day02 {
         String[] turn;
         String input2;
         int id;
-        int sum = 0;
+        long sum = 0;
         while (scanner.hasNextLine()) {
-            boolean gameLagit = true;
+
+            int redMinCount = 0;
+            int greenMinCount = 0;
+            int blueMinCount = 0;
+
             input2 = scanner.nextLine().replace("Game ", "").replace(": ", ";");
             input = input2.split(";");
-            id = Integer.parseInt(input[0]);
+
             for (int i = 1; i < input.length; i++) {
-                int redCount = 0;
-                int greenCount = 0;
-                int blueCount = 0;
                 String[] game = input[i].split(", ");
                 for (int j = 0; j < game.length; j++) {
                     game[j] = game[j].trim();
                 }
-                for (int j = 0; j < game.length; j++) {
-                    turn = game[j].split(" ");
+                for (String s : game) {
+                    turn = s.split(" ");
                     if (turn[1].equals("red")) {
-                        redCount = Integer.parseInt(turn[0]);
-                        if (redCount > 12) {
-                            gameLagit = false;
-                            break;
-                        }
+                        redMinCount = Math.max(Integer.parseInt(turn[0]), redMinCount);
                     } else if (turn[1].equals("green")) {
-                        greenCount = Integer.parseInt(turn[0]);
-                        if (greenCount > 13) {
-                            gameLagit = false;
-                            break;
-                        }
+                        greenMinCount = Math.max(Integer.parseInt(turn[0]), greenMinCount);
                     } else if (turn[1].equals("blue")) {
-                        blueCount = Integer.parseInt(turn[0]);
-                        if (blueCount > 14) {
-                            gameLagit = false;
-                            break;
-                        }
+                        blueMinCount = Math.max(Integer.parseInt(turn[0]), blueMinCount);
                     }
                 }
-                if (!gameLagit) {
-                    break;
-                }
             }
-            if (gameLagit) {
-                sum += id;
-            }
+            sum += (long) redMinCount * greenMinCount * blueMinCount;
         }
         System.out.println(sum);
     }
